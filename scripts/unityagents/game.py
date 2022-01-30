aefb2a4faf91835e5b44b0324b76e9321399a1b9
brabeem <brabeem123@gmail.com>
2022-01-30 14:28:05 +0545

    combined all functionalities to make it look like gym

--- scripts/unityagents/game.py
+++ scripts/unityagents/game.py
@@ -1,19 +1,12 @@
+from turtle import done
 from agents import dodgeball_agents
 import numpy as np
-if '__main__' == __name__:
-    dodgeBall = dodgeball_agents("/home/love/Documents/ctfvideo/ctfvideo.x86_64")
-    dodgeBall.set_env()
-    print(dodgeBall.action_size())
-    print(dodgeBall.nbr_agent)
-    c=np.array([[0,0,1],[0,0,1],[0,0,1]])
-    d=np.array([[0,0],[0,0],[0,0]])
-    agent_id=[0,1,2] ##agent_id is the index of the agent in the team
-    team_id=[0,1] ##team_id is the index of the team
+if __name__ == "__main__":
+    env = dodgeball_agents("/home/brabeem/Documents/deepLearning/builds/singenv/sectf.x86_64")
+    env.set_env()
+    state = env.reset()
     for i in range(100):
-        dodgeBall.set_action_for_agent(team_id[1],agent_id[0],np.array([[0.1,0,0]]),np.array([[0,1]]))
-        #dodgeBall.set_action_for_team(team_id[0],c,d)
-        dodgeBall.set_step(team_id[0])
-        print(dodgeBall.get_agent_obs_from_decision_steps(dodgeBall.decision_steps,team_id[0], agent_id[0]))
-        #print(dodgeBall.reward_and_terminalstate_for_agent(team_id[1],agent_id[0]))
-        dodgeBall.env.reset()
-    dodgeBall.env_close()
+        actions  = env.random_action()
+        next_state,reward,done = env.step(actions)
+        next_state = state
+    env.close()
