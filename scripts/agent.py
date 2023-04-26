5779c58d80c2937e772edece69efe85cdfd91ec9
Love Panta <54012619+love481@users.noreply.github.com>
2023-04-26 13:31:46 +0545

    Update agent.py

--- scripts/agent.py
+++ scripts/agent.py
@@ -17,43 +17,16 @@ class Agent(nn.Module):
         #self.seed = random.seed(89)
 
     def select_action(self, o, noise_rate, epsilon):
-        #if np.random.uniform() < epsilon or 1:
-        if 1:
-        #     a= np.random.uniform(-1,1, self.args.action_shape[self.agent_id])
-        # else:
-            inputs = torch.tensor(o, dtype=torch.float32).unsqueeze(0).to(self.args.device)
-            self.policy.actor_network.eval()
-            with torch.no_grad():        
-                pi = self.policy.actor_network(inputs).squeeze(0).detach().to(self.args.device)
-            self.policy.actor_network.train()
-            if noise_rate !=0:
-                pi = pi + self.noise.sample()
-            a=pi.cpu().numpy()
-            # if noise_rate !=0:
-            #    a += self.noise.sample()
-            # noise = noise_rate * self.args.high_action * np.random.randn(*a.shape)  # gaussian noise
-            # a += noise
-            a = np.clip(a, -1, 1)
-            #a=torch.softmax(torch.tensor(a).float().to(self.args.device),dim=0).cpu().numpy()
-            # a[self.args.continuous_action_space:]=torch.softmax(torch.tensor(a[self.args.continuous_action_space:]).float().to(self.args.device),dim=0).cpu().numpy()
-            #a[:self.args.continuous_action_space]=self.args.high_action*torch.softmax(torch.tensor(a[:self.args.continuous_action_space]).float().to(self.args.device)).cpu().numpy()
-        # c_a=torch.softmax(torch.tensor(a[:self.args.continuous_action_space]).float(),dim=0)
-        # a[:self.args.continuous_action_space]=c_a.cpu().numpy()
-        #d_a=torch.argmax(torch.softmax(torch.tensor(a[self.args.continuous_action_space:]).float(),dim=0))
-        # d_a=torch.sigmoid(torch.tensor(a[self.args.continuous_action_space:]).float().to(self.args.device))
-        # d_a=d_a.cpu().numpy()
-        # d_a=np.random.binomial(1,d_a)
-        # for i in range(self.args.discrete_action_space):
-        #     if i==d_a:
-        #         a[int(self.args.continuous_action_space+d_a)]=1
-        #     else:
-        #         a[int(self.args.continuous_action_space+i)]=0
-        #a[:self.args.continuous_action_space]= a[:self.args.continuous_action_space]
-       # a[2]=0.5*a[2]
-        #a[self.args.continuous_action_space:]=np.random.binomial(1,np.abs(a[self.args.continuous_action_space:]))
+        inputs = torch.tensor(o, dtype=torch.float32).unsqueeze(0).to(self.args.device)
+        self.policy.actor_network.eval()
+        with torch.no_grad():        
+            pi = self.policy.actor_network(inputs).squeeze(0).detach().to(self.args.device)
+        self.policy.actor_network.train()
+        if noise_rate !=0:
+            pi = pi + self.noise.sample()
+        a=pi.cpu().numpy()
+        a = np.clip(a, -1, 1)
         a[self.args.continuous_action_space:]=(np.abs(a[self.args.continuous_action_space:])>0.3)*1
-        # if(self.agent_id==0):
-        #     print(a)
         return a.copy()
 
 
